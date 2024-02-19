49071a451e8f0a5e2925b4dffb23a94fba0416ab
reden21345 <mirandilla395@gmail.com>
2024-02-19 15:36:58 +0800

    emKit Model

---
+++ models/emKit.js
@@ -0,0 +1,62 @@
+const mongoose = require('mongoose');
+
+const emKitSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, 'Please enter Emergency item name'],
+        trim: true,
+        maxLength: [100, 'Em item name cannot exceed 100 characters']
+    },
+    percentage: {
+        type: Number,
+        required: [true, 'Please enter Emergency Item recommended percentage'],
+        maxLength: [5, 'Em Item name cannot exceed 5 characters'],
+        default: 0.0
+    },
+    description: {
+        type: String,
+        required: [true, 'Please enter Emergency Item description'],
+    },
+    images: [
+        {
+            public_id: {
+                type: String,
+                required: true
+            },
+            url: {
+                type: String,
+                required: true
+            },
+        }
+    ],
+    category: {
+        type: String,
+        required: [true, 'Please select category for this Emergency item'],
+        enum: {
+            values: [
+                'Fire',
+                'Earthquake',
+                'Typhoon',
+                'Flood'
+            ],
+            message: 'Please select correct category for Emergency item'
+        }
+    },
+    stock: {
+        type: Number,
+        required: [true, 'Please enter coffee stock'],
+        maxLength: [5, 'Coffee name cannot exceed 5 characters'],
+        default: 0
+    },
+    // user: {
+    //     type: mongoose.Schema.ObjectId,
+    //     ref: 'User',
+    //     required: true
+    // },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+})
+
+module.exports = mongoose.model('emKits', emKitSchema)
